4427ff24c914d31bdda1c6d60cd82a836c5a33d9
Roman Nikolaev <nikolaev.rd@yandex.ru>
2022-04-24 15:19:26 +0300

    Добавлена инструкция про Native-режим (#56)
    * Удаление Gemfile.lock перед локальным запуском
    * Добавлена инструкция про Native-режим

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Cleanup: remove static site folder and metadata..."
-rm -rf _site .jekyll-metadata
+rm -rf _site .jekyll-metadata Gemfile.lock
 
 echo "Launch site in Docker container..."
 docker run --rm \
